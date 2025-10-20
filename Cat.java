e853f55c23a360ee2fe0be25c4297265287b3ff2
Anastaisha18 <nastyvavilina@gmail.com>
2025-10-20 06:40:15 +0500

    Add files via upload

---
+++ Cat.java
@@ -0,0 +1,28 @@
+// Cat.java - класс кота
+public class Cat {
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public void meow() {
+        System.out.println(name + ": мяу!");
+    }
+
+    public void meow(int times) {
+        System.out.print(name + ": ");
+        for (int i = 0; i < times; i++) {
+            System.out.print("мяу");
+            if (i < times - 1) {
+                System.out.print("-");
+            }
+        }
+        System.out.println("!");
+    }
+
+    @Override
+    public String toString() {
+        return "кот: " + name;
+    }
+}
